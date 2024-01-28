4bb9eedd053c154e0945c12daeef83cb0269f6bd
rohsaurus <rohanparikh04@gmail.com>
2024-01-28 08:52:43 -0500

    given up on tflite - model was trained with numpy arrays which isnt supported on tflite

--- google_map_starting/lib/model.dart
+++ google_map_starting/lib/model.dart
@@ -1,53 +1,64 @@
+import "dart:io";
 import "dart:typed_data";
 import "package:camera/camera.dart";
 import "package:flutter/material.dart";
 import "package:image/image.dart" as img;
 import "package:tflite_flutter/tflite_flutter.dart";
 
-
 class Model extends StatefulWidget {
-  final imgPath;
+  final String imgPath;
   const Model({super.key, required this.imgPath});
   @override
   State<Model> createState() => _ModelState();
 }
 
 class _ModelState extends State<Model> {
-  Future<void> classifyImage(Interpreter interpreter) async {
-    //loading the image from file
-    var image = img.decodeImage(widget.imgPath.readAsBytesSync());
-    //resizing the image to be a 256x256 image as the model only accepts that size by manipulating the bytes
-    var height = image!.height;
-    var width = image.width;
-    Uint32List input = Uint32List(256 * 256);
-    var index = 0;
-    for (var i = 0; i < 256; i++) {
-      for (var j = 0; j < 256; j++) {
-        var pixel = image.getPixel(j, i);
-        input[index] = pixel as int;
-        index++;
-      }
-    }
+  late Interpreter interpreter;
+  Future<void> classifyImage() async {
+    // Load image from path and convert it to bytes and resize to 256x256 for our model input
+    var resizedImg = Image.file(File(widget.imgPath), width: 256, height: 256);
+    runInference(resizedImg);
+  }
 
 
-    final output = List<List<dynamic>>.filled(1, List<dynamic>.filled(1001, 0));
+  Future<List> runInference(Image image) async {
+    // Preprocess the input image
+    
+
+    // Allocate memory for the output
+    final output = List<double>.filled(1, 0).reshape([1, 1]);
+
+    // Run inference
     interpreter.run(input, output);
-  
-    print(output);
 
+    return output;
   }
-  @override
-  Future<void> initState() async {
-    // TODO: implement initState
-    super.initState();
-    final interpreter = await Interpreter.fromAsset('assets/model.tflite');
-    classifyImage(interpreter);
-    interpreter.run;
+
+  Future<String> loadModel() async {
+    interpreter = await Interpreter.fromAsset('assets/model.tflite');
+    return classifyImage();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        
+    return FutureBuilder<Interpreter>(
+      future: loadModel(),
+      builder: (BuildContext context, AsyncSnapshot<Interpreter> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          classifyImage(snapshot.data!);
+          return Scaffold(
+              // Your other widgets here...
+              );
+        }
+      },
     );
   }
 }
