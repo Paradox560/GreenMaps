870283fc0f44351c3e957b842c3033734138c733
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-01-28 01:29:37 -0500

    made design changes to the camera screen

--- google_map-Starting/lib/camera.dart
+++ google_map-Starting/lib/camera.dart
@@ -57,19 +57,29 @@ class _CameraAppState extends State<CameraApp> {
       return Container();
     }
     return Scaffold(
-      appBar: AppBar(title: Text('Camera Example')),
-      body: CameraPreview(_controller),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera),
-        onPressed: () async {
-          try {
-            // Take a picture and save it to the gallery.
-            final image = await _controller.takePicture();
-            print('Image saved to gallery: ${image.path}');
-          } catch (e) {
-            print('Error taking picture: $e');
-          }
-        },
+      appBar: AppBar(title: Text('Product Disposal Identification')),
+      body: Column(
+        children: [
+          CameraPreview(_controller),
+          SizedBox(height: 48),
+          SizedBox(
+            height: 80,
+            width: 80,
+            child:
+            FloatingActionButton(
+              child: Icon(Icons.camera, size: 36),
+              onPressed: () async {
+                try {
+                  // Take a picture and save it to the gallery.
+                  final image = await _controller.takePicture();
+                  print('Image saved to gallery: ${image.path}');
+                } catch (e) {
+                  print('Error taking picture: $e');
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
